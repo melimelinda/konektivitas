69d59a104bf9a549aa7cf9d775d8046cdc9123e6
Meli Melinda <melinawada29@gmail.com>
2018-11-06 21:46:32 +0700

    ref2

---
+++ hasilsearch.php
@@ -0,0 +1,21 @@
+<?php 
+	$kolom =$_POST['kolom'];
+	$cari =$_POST['cari'];
+	$conn =mysqli_connect("localhost","root","");
+	mysqli_select_db("daftartamu",$conn);
+	$hasil=mysqli_query("select * from bukutsmu where $kolom like '%$cari%'", $conn);
+	echo "<br>";
+	echo "Ditemukan : $jumlah";
+	echo "<br>";
+	while ($baris=mysql_fetch_array($hasil))
+	{
+		echo "Nama : ";
+		echo $baris[0];
+		echo "<br>";
+		echo "Email : ";
+		echo $baris[1];
+		echo "<br>";
+		echo "Komentar : ";
+		echo $baris[2];
+	}
+ ?>
